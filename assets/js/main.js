7ecd381fdba2084eeee3c215bbb90097be65df94
Nayem Ahmed <nayem1145@gmail.com>
2024-09-18 00:44:39 +0600

    updated situaations.me

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,105 @@
             });
         }
 
+        // Contact Form Submission
+        $('#contact_form').on('submit', function (e) {
+            e.preventDefault();
+            var $this = $(this);
+         
+            $('button[type="submit"]', this).attr('disabled', 'disabled').val('Processing...');
+            var form_data = $this.serialize();
+            var required = 0;
+            $(".required", this).each(function () {
+                  if ($(this).val() === ''){
+                     $(this).addClass('reqError');
+                     required += 1;
+                  } else{
+                     if ($(this).hasClass('reqError'))
+                     {
+                        $(this).removeClass('reqError');
+                        if (required > 0)
+                        {
+                              required -= 1;
+                        }
+                     }
+                  }
+            });
+            if (required === 0) {
+                  $.ajax({
+                     type: 'POST',
+                     url: 'ajax/mail.php',
+                     data: {form_data: form_data},
+                     success: function (data) {
+                        $('button[type="submit"]', $this).removeAttr('disabled').val('Message');
+         
+                        $('.con_message', $this).fadeIn().html('<strong>Congratulations!</strong> Your Question has been submitted.').removeClass('alert-warning').addClass('alert-success');
+                        setTimeout(function () {
+                              $('.con_message', $this).fadeOut().html('').removeClass('alert-success alert-warning');
+                        }, 5000);
+                     }
+                  });
+            } else {
+                  $('button[type="submit"]', $this).removeAttr('disabled').val('Message');
+                  $('.con_message', $this).fadeIn().html('<strong>Opps!</strong> Errpr found. Please fix those and re submit.').removeClass('alert-success').addClass('alert-warning');
+                  setTimeout(function () {
+                     $('.con_message', $this).fadeOut().html('').removeClass('alert-success alert-warning');
+                  }, 5000);
+            }
+         });
+         $(".required").on('keyup', function () {
+            $(this).removeClass('reqError');
+         });
+      
+      
+         $('.input_field input, .input_field textarea').val("");
+         $('.input_field input, .input_field textarea').focusout(function() {
+            var text_val = $(this).val();
+            if (text_val === "") {
+               console.log("empty!");
+               $(this).removeClass('has-value');
+            } else {
+               $(this).addClass('has-value');
+            }
+        });
 
     });
+
+    // Section scroll Id
+    // $(".st_header_nav .scroll").on("click", function (event) {
+        
+    //     $("html,body").animate({
+    //         scrollTop: $(this.hash).offset().top - 80
+    //     });
+    // });
+
+    // $(".main_mobile_menu .scroll").on("click", function (event) {
+        
+    //     $("html,body").animate({
+    //         scrollTop: $(this.hash).offset().top - 80
+    //     });
+    // });
+
+
+    $('.scroll').on('click', function() {
+        
+        
+
+        // Get the target section ID from the link's href
+        var target = $(this.hash);
+
+        // Scroll to the target section smoothly
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - 100
+        }, 800);
+    });
+
+    if (window.location.hash) {
+        // Scroll to the target section smoothly after the page has loaded
+        $('html, body').animate({
+            scrollTop: $(window.location.hash).offset().top - 100
+        }, 800);
+    }
+
+
 
 })(jQuery);
